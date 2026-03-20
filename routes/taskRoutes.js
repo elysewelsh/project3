f6000acd1cbac48f9b929d13cf45eeec06867688
elysewelsh <elysecwelsh@gmail.com>
2026-03-20 15:48:34 -0400

    task passed read tasks test plan

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -11,18 +11,31 @@ router.use(authMiddleware);
 router.post('/:projectId/tasks', async (req, res) => {
   try {
     const project = await Project.findById(req.params.projectId);
-    const user = req.user._id;
-    if (project.user != user) {
+    if (project.user != req.user._id) {
         return res.status(403).json({ message: 'User forbidden from updating this project' });
     }
     const task = new Task({
       ...req.body,
-      project: project
+      project: req.params.projectId
     });
     await task.save();
     res.status(201).json(task);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+router.get('/projects/:projectId/tasks', async (req, res) => {
+  try {
+    const project = await Project.findById(req.params.projectId);
+       if (project.user != req.user._id) {
+        return res.status(403).json({ message: 'User forbidden from accessing this project' });
+    }
+    const tasks = await Task.find(
+        {project: { $eq: req.params.projectId}});
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
